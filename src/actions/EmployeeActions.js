cfa268f55462731da4e7869a1f72bb7e59c30260
Vishal Karande <vishalkarande4@gmail.com>
2018-09-16 14:32:29 -0400

    done with showing list of employees

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,5 +1,10 @@
+import firebase from 'firebase';
+import {Actions} from 'react-native-router-flux';
+
 import {
-    EMPLOYEE_UPDATE
+    EMPLOYEE_UPDATE,
+    EMPLOYEE_CREATE,
+    EMPLOYEES_FETCH_SUCCESS
    } from './types';
 
 export const employeeUpdate = ({prop,value}) => {
@@ -8,3 +13,25 @@ export const employeeUpdate = ({prop,value}) => {
         payload: { prop, value}
     };
 };
+
+export const employeeCreate = ({name,phone,shift}) => {
+    const { currentUser } = firebase.auth();
+    return(dispatch) =>{
+     firebase.database().ref(`/users/${currentUser.uid}/employees`)
+        .push({ name, phone, shift })
+        .then(() =>{
+            dispatch({type: EMPLOYEE_CREATE});
+            Actions.employeeList({type:'reset'});
+        });
+    };    
+};
+
+export const employeeFetch = () => {
+    const { currentUser } = firebase.auth();
+    return (dispatch) =>{
+        firebase.database().ref(`/users/${currentUser.uid}/employees`)
+            .on('value', snapshot => {
+                dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });    
+            });
+    };
+};    
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,14 @@
 import React , {Component } from 'react';
 import {Picker,Text} from 'react-native';
 import {connect} from 'react-redux';
-import {employeeUpdate} from '../actions';
+import {employeeUpdate,employeeCreate} from '../actions';
 import {Card, CardSection, Input, Button} from './common';
 
 class EmployeeCreate extends Component {
+    onButtonPress() {
+        const {name,phone,shift} = this.props;
+        this.props.employeeCreate({name,phone,shift : shift || 'Monday'});
+    }
 
     render(){
         return(
@@ -23,7 +27,7 @@ class EmployeeCreate extends Component {
                         label="Phone"
                         placeholder="555-555-5555"
                         value={this.props.phone}
-                        onChangeText={text => this.props.employeeUpdate({prop:'name',value:text})}
+                        onChangeText={text => this.props.employeeUpdate({prop:'phone',value:text})}
                     /> 
                 </CardSection>
 
@@ -46,7 +50,7 @@ class EmployeeCreate extends Component {
                 </CardSection>
 
                 <CardSection>
-                    <Button>
+                    <Button onButtonPress={this.onButtonPress.bind(this)}>
                         Create
                     </Button>    
                 </CardSection>
@@ -66,4 +70,4 @@ const mapStateToProps = (state) => {
 };
 
 
-export default connect(mapStateToProps, {employeeUpdate}) (EmployeeCreate);
+export default connect(mapStateToProps, {employeeUpdate,employeeCreate}) (EmployeeCreate);
